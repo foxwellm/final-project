a53b137b960c7897eec6da09b226b2d8bec27d9e
Matthew Foxwell <foxwellmw@gmail.com>
2019-04-16 08:41:36 -0600

    Add functionality to display multiple images

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img v-for="image in results" :src="image.baseimageurl" v-bind:key="image.baseimageurl">
     <router-view/>
   </div>
 </template>
 
 <script>
+import API_KEYS from "../API_KEYS";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      results: []
+    };
+  },
+  async mounted() {
+    try {
+      const response = await fetch(
+        `https://api.harvardartmuseums.org/image?sort=random&q=height:800&apikey=${API_KEYS.Harvard}`
+      );
+      const result = await response.json();
+      this.results = result.records;
+      console.log(this.results[0]);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import API_KEYS from '../API_KEYS'
 
 Vue.config.productionTip = false
 
@@ -13,16 +12,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>',
-  data: {
-    results: []
-  },
-  async mounted() {
-    try {
-      const response = await fetch(`https://api.harvardartmuseums.org/person?q=culture:Dutch&apikey=${API_KEYS.Harvard}`)
-      const result = await response.json()
-      this.results = result.records
-    } catch (error) {
-      console.log(error.message)
-    }
-  }
 })
