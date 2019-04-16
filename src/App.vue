06410685b5a4403d6899da5a856218623296d1a7
Matthew Foxwell <foxwellmw@gmail.com>
2019-04-16 14:36:45 -0600

    Add display css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img v-for="image in results" :src="image.baseimageurl" v-bind:key="image.baseimageurl">
-    <router-view/>
+    <div class="wall-background"></div>
+    <div class="art-container">
+    <img class="art-image" v-for="image in results" :src="image.baseimageurl" v-bind:key="image.baseimageurl">
+    </div>
   </div>
 </template>
 
@@ -18,7 +20,7 @@ export default {
   async mounted() {
     try {
       const response = await fetch(
-        `https://api.harvardartmuseums.org/image?sort=random&q=height:800&apikey=${API_KEYS.Harvard}`
+        `https://api.harvardartmuseums.org/image?sort=random&q=height:800&width:1024&size=3&apikey=${API_KEYS.Harvard}`
       );
       const result = await response.json();
       this.results = result.records;
@@ -37,6 +39,27 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.wall-background {
+  content:url('./assets/wall2.jpg');
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+.art-container {
+  width: 83%;
+  margin: auto;
+  display: flex;
+  justify-content: space-around;
+}
+
+.art-image {
+  width: 25%;
+  height: 20%;
+  margin: 11.5% 5%;
 }
 </style>
